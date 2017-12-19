f56276c4625390f3c749f3f26d4a0e6d4d235551
Saverio Ferrara <fsferrara@gmail.com>
2017-12-19 02:40:33 +0100

    example with two resolvers

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,10 +1,17 @@
 const reservationServiceClient = require('../services/ReservationService');
+const hotelServiceClient = require('../services/HotelService');
 
 module.exports = {
   QueryType: {
     reservations: (context, data) => (function(context, data) {
       console.log("[ReservationService] getting reservations list for the user id: " + data.userId);
       return reservationServiceClient.getReservations(data.userId);
+    })(context, data),
+  },
+  ReservationType: {
+    hotel: (context, data) => (function(context, data) {
+      console.log("[HotelService] getting details for hotel id: " + context.hotel);
+      return hotelServiceClient.getHotel(context.hotel);
     })(context, data),
   }
 };
